0bf9b1e2362cdb45352aff391ba0582403f5bdd5
QQianway <qqianway@gmail.com>
2017-11-16 17:04:27 +0800

    Add files via upload

---
+++ web/dist/js/extra.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function($) {
+    $(".clickable-row").click(function() {
+        window.location = $(this).data("href");
+    });
+});
+
+function hideDiv(select){
+    if(select==1){
+         document.getElementById("form1").style.display="block";
+         document.getElementById("form2").style.display="none";
+          document.getElementById('choose').disabled=true;
+    }
+    
+    else if(select==2){
+         document.getElementById("form2").style.display="block";
+         document.getElementById("icon2").style.display="block";
+         document.getElementById("form1").style.display="none";
+         document.getElementById('choose').disabled=true;
+    }
+}
+
+function addClass(div,i,classname){
+    var node = document.getElementById(div).getElementsByTagName("li")[i];
+    node.setAttribute("class", classname);
+}
+
+function addClass(div,i,classname,j,k){
+    var node = document.getElementById(div).getElementsByTagName("li")[i];
+    node.setAttribute("class", classname);
+    
+    var node2 = document.getElementById(div).getElementsByTagName("li")[j];
+    node2.setAttribute("class", "");
+    
+    var node3 = document.getElementById(div).getElementsByTagName("li")[k];
+    node3.setAttribute("class", "");
+}
+
+//triggered when modal is about to be shown
+$('#modal-edit').on('show.bs.modal', function(e) {
+
+    //get data-id attribute of the clicked element
+    var carId = $(e.relatedTarget).data('car-id');
+
+    //populate the textbox
+    $(e.currentTarget).find('input[name="carID"]').val(carId);
+});
